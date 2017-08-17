76f1eda28696ebb88fe9532d0f13a0d31cca2715
avenirs05 <avenirs05@gmail.com>
2017-08-17 18:29:08 +0300

    last

--- views/user/register.php
+++ views/user/register.php
@@ -4,8 +4,8 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-4 col-sm-offset-4 padding-right">                
-                
-				<?php if ($result): ?>
+
+                <?php if ($result): ?>
                     <p>Вы зарегистрированы!</p>
                 <?php else: ?>
                     <?php if (isset($errors) && is_array($errors)): ?>
@@ -14,19 +14,19 @@
                                 <li> - <?php echo $error; ?></li>
                             <?php endforeach; ?>
                         </ul>
-                <?php endif; ?>
+                    <?php endif; ?>
 
                     <div class="signup-form"><!--sign up form-->
-                        <h2>Регистрация на сайте</h2>
+                        <h2>Заполните данные ниже</h2>
                         <form action="#" method="post">
                             <input type="text" name="name" placeholder="Имя" value="<?php echo $name; ?>"/>
                             <input type="email" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
                             <input type="password" name="password" placeholder="Пароль" value="<?php echo $password; ?>"/>
-                            <input type="submit" name="submit" class="btn btn-default" value="Регистрация" />
+                            <input type="submit" name="submit" class="btn btn-default" value="Отправить" />
                         </form>
-                    </div><!--/sign up form-->
-                
-                <?php //endif; ?>
+                    </div><!--/sign up form-->                
+                <?php endif; ?>
+
                 <br/>
                 <br/>
             </div>
@@ -34,4 +34,6 @@
     </div>
 </section>
 
+
 <?php include ROOT . '/views/layouts/footer.php'; ?>
+
